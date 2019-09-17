8baa9623b8050a3d86c918903587389c0117c3e8
Serifhan <serifhaniskl@gmail.com>
2019-09-17 09:49:38 +0300

    Basic App Like Medium

---
+++ app/src/main/java/com/example/communityblog/Activities/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.communityblog.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.communityblog.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
